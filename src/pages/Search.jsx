86d535665cd54aefec73bb609a2045d57f5a930c
caue peruque <caue19052003@gmail.com>
2023-04-14 00:44:10 -0300

    requisito 6 finalizado

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import Header from '../components/Header/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from '../components/Loading/Loading';
 
 class Search extends Component {
   state = {
     isDisable: true,
+    isLoading: false,
     searchValue: '',
+    savedNameArtist: '',
+    hasAlbum: false,
+    returnAPI: '',
   };
 
   validationInput = () => {
@@ -13,40 +20,113 @@ class Search extends Component {
     const valSearch = searchValue.length >= minLength;
     this.setState({
       isDisable: !valSearch,
+      // success: false,
     });
   };
 
-  handleChange = ({ target: { name, value } }) => {
+  handleChange = ({ target: { value } }) => {
     this.setState({
-      [name]: value,
+      searchValue: value,
     }, this.validationInput);
   };
 
-  render() {
-    const { isDisable } = this.state;
-    return (
+  saveName = () => {
+    this.setState((prevState) => ({
+      savedNameArtist: prevState.searchValue,
+    }));
+  };
 
+  handleClick = async (event) => {
+    event.preventDefault();
+    const { searchValue } = this.state;
+    this.setState({
+      isLoading: true,
+    });
+    const returnAPI = await searchAlbumsAPI(searchValue);
+    this.saveName();
+    this.setState({
+      isLoading: false,
+      searchValue: '',
+      returnAPI,
+      hasAlbum: !returnAPI.length === 0,
+    });
+  };
+
+  render() {
+    const {
+      isDisable,
+      isLoading,
+      searchValue,
+      savedNameArtist,
+      returnAPI,
+      hasAlbum,
+    } = this.state;
+
+    return (
       <div data-testid="page-search">
         <Header />
-
-        <form>
-          <label htmlFor="input-search">
-            <input
-              type="text"
-              name="searchValue"
-              id="input-search"
-              data-testid="search-artist-input"
-              onChange={ this.handleChange }
-            />
-          </label>
-          <button data-testid="search-artist-button" disabled={ isDisable }>
-            Pesquisar
-          </button>
-
-        </form>
-
+        { isLoading ? (
+          <Loading />
+        ) : (
+          <>
+            <form>
+              <label htmlFor="input-search">
+                <input
+                  type="text"
+                  name="searchValue"
+                  id="input-search"
+                  data-testid="search-artist-input"
+                  onChange={ this.handleChange }
+                  value={ searchValue }
+                />
+              </label>
+              <button
+                data-testid="search-artist-button"
+                disabled={ isDisable }
+                onClick={ this.handleClick }
+              >
+                Pesquisar
+              </button>
+            </form>
+            {returnAPI && returnAPI.length > 0 && (
+              <>
+                <p>{`Resultado de álbuns de: ${savedNameArtist} `}</p>
+                { returnAPI.map(({
+                  artistName,
+                  collectionId,
+                  collectionName,
+                  artworkUrl100,
+                }) => (
+                  <>
+                    <img
+                      key={ collectionId }
+                      src={ artworkUrl100 }
+                      alt={ `Imagem do album ${collectionName} de ${artistName}` }
+                    />
+                    <h3>
+                      { collectionName }
+                    </h3>
+                    <p>
+                      { artistName }
+                    </p>
+                    <div>
+                      <Link
+                        to={ `/album/${collectionId}` }
+                        data-testid={ `link-to-album-${collectionId}` }
+                      >
+                        Clique aqui para acessar o albúm
+                      </Link>
+                    </div>
+                  </>
+                ))}
+              </>
+            )}
+            {!hasAlbum && returnAPI && returnAPI.length === 0 && (
+              <p>Nenhum álbum foi encontrado</p>
+            )}
+          </>
+        )}
       </div>
-
     );
   }
 }
