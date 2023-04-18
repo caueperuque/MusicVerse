3f2ee7da82b147e4234b834bc8732b7356d4162b
caue peruque <caue19052003@gmail.com>
2023-04-17 21:10:48 -0300

    css: add style for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import { getUser } from '../../services/userAPI';
 import Loading from '../Loading/Loading';
+import './Header.css';
 
 class Header extends Component {
   state = {
@@ -20,21 +21,59 @@ class Header extends Component {
   render() {
     const { user, isLoading } = this.state;
     return (
-      <header data-testid="header-component">
+      <header data-testid="header-component" className="box" id="header">
 
         {
           isLoading ? (
             <Loading />
           ) : (
-            <h2 data-testid="header-user-name">
-              { user }
-            </h2>
+            <section id="header__section">
+              <div>
+                <h2
+                  data-testid="header-user-name"
+                  className="title is-4"
+                  id="header__title"
+                >
+                  {`Welcome,
+                  ${user}
+                  !`}
+                </h2>
+              </div>
+              <article className="header__container">
+                <nav className="navbar__header">
+                  <div className="nav__container-items">
+                    <div className="nav__items">
+                      <Link to="/favorites" data-testid="link-to-favorites">
+                        <span className="nav__span">
+                          Favorites
+                        </span>
+                      </Link>
+                    </div>
+                    <div className="nav__items">
+                      <Link to="/profile" data-testid="link-to-profile">
+                        <span className="nav__span">
+                          Profile
+                        </span>
+                      </Link>
+                    </div>
+                    <div
+                      className="nav__items"
+                    >
+                      <Link to="/search" data-testid="link-to-search">
+                        <span className="nav__span">
+                          Search
+                        </span>
+                      </Link>
+                    </div>
+                  </div>
+                </nav>
+              </article>
+              {/* <hr /> */}
 
+            </section>
           )
         }
-        <Link to="/search" data-testid="link-to-search">Search</Link>
-        <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
-        <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+
       </header>
     );
   }
