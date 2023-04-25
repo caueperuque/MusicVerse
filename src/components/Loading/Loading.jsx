48fd058579497311662fb2dacc5c11bb82451b3c
caue peruque <caue19052003@gmail.com>
2023-04-25 11:44:51 -0300

    add loading animation

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 class Loading extends Component {
   render() {
     return (
       <div>
-        <h1>
+        {/* <h1>
           Carregando...
-        </h1>
+        </h1> */}
+        <ClipLoader
+          // cssOverride={ this.override }
+          data-testid="loader"
+        />
       </div>
     );
   }
