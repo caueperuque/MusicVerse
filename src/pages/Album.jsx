41b3769fefdcd82d110f99877296a643fd1ee91d
caue peruque <caue19052003@gmail.com>
2023-04-25 12:11:30 -0300

    add img in album collection

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -11,12 +11,14 @@ class Album extends Component {
     bandName: '',
     albumName: '',
     albumMusics: '',
+    albumImg: '',
     isLoading: false,
   };
 
   async componentDidMount() {
     const { match: { params: { id } } } = this.props;
     const musics = await getMusics(id);
+    const imgAlbum = musics.map(({ artworkUrl100 }) => artworkUrl100)[0];
     const band = musics.map(({ artistName }) => artistName)[0];
     const colectionName = musics
       .map(({ collectionCensoredName }) => collectionCensoredName)[0];
@@ -24,23 +26,32 @@ class Album extends Component {
       bandName: band,
       albumName: colectionName,
       albumMusics: musics,
+      albumImg: imgAlbum,
     });
   }
 
   render() {
-    const { bandName, albumName, albumMusics, isLoading } = this.state;
+    const { bandName, albumName, albumMusics, isLoading, albumImg } = this.state;
     return (
 
       <div data-testid="page-album">
         <Header />
         { bandName && (
-          <div className="album__title">
-            <h2 data-testid="album-name" className="subtitle is-3">
-              { albumName }
-            </h2>
-            <h1 data-testid="artist-name" className="subtitle is-4">
-              { bandName }
-            </h1>
+          <div>
+            <div id="album__img">
+              <img
+                src={ albumImg.replaceAll('100x100bb', '200x200bb') }
+                alt={ albumName }
+              />
+            </div>
+            <div className="album__title">
+              <h2 data-testid="album-name" className="subtitle is-3">
+                { albumName }
+              </h2>
+              <h1 data-testid="artist-name" className="subtitle is-4">
+                { bandName }
+              </h1>
+            </div>
           </div>
         )}
         { isLoading ? (
